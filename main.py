9661b5249cc5308e19a63929d901d7a671af3647
AydenHocking <hockinga2004@gmail.com>
2025-02-09 18:07:25 -0500

    feat(method): add make_sandwich method

--- main.py
+++ main.py
@@ -76,5 +76,8 @@ class SandwichMachine:
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
            Hint: no output"""
+        for item, amount in order_ingredients.items():                                              ##Iterates through the items in the selected sandwich order.
+            self.machine_resources[item] -= amount                                                  ##Remove Ingredients.
+        print(str(sandwich_size) + " sandwich is ready. Bon appetit!")
 
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
